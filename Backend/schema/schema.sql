f5eced842f13f421cc0d4ae570e0de341a6e2789
Sarthak12903 <sarthak12903@gmail.com>
2025-01-25 13:46:01 +0530

    minor

--- Backend/schema/schema.sql
+++ Backend/schema/schema.sql
@@ -16,3 +16,25 @@ CREATE TABLE "user" (
     role VARCHAR NOT NULL,
     status VARCHAR NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS public."addBook"
+(
+    unique_id character varying COLLATE pg_catalog."default" NOT NULL,
+    title character varying COLLATE pg_catalog."default",
+    author character varying COLLATE pg_catalog."default",
+    isbn character varying COLLATE pg_catalog."default",
+    copies character varying COLLATE pg_catalog."default",
+    CONSTRAINT "addBook_pkey" PRIMARY KEY (unique_id)
+)
+
+
+CREATE TABLE IF NOT EXISTS public."borrowBook"
+(
+    unique_id character varying COLLATE pg_catalog."default" NOT NULL,
+    title character varying COLLATE pg_catalog."default" NOT NULL,
+    author character varying COLLATE pg_catalog."default" NOT NULL,
+    isbn character varying COLLATE pg_catalog."default" NOT NULL,
+    "borrowDate" character varying COLLATE pg_catalog."default" NOT NULL,
+    "dueDate" character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT "borrowBook_pkey" PRIMARY KEY (unique_id)
+)
